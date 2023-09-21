341de7fd707c6c788c1e3827bfc2232a5d030fd3
Richard Berry <rjsberry@pm.me>
2023-09-21 23:37:20 +0100

    basic blocking impl

--- readme.md
+++ readme.md
@@ -3,3 +3,14 @@
 A Real-Time Transfer (target side) implementation in Zig.
 
 Dual licensed under the 0BSD and MIT licenses.
+
+## Usage
+
+```zig
+const rtt = @import("rtt");
+
+export fn main(void) callconv(.C) noreturn {
+    rtt.println("Hello from {s}", .{"Zig!"});
+    while (true) {}
+}
+```
--- rtt.zig
+++ rtt.zig
@@ -1,10 +1,60 @@
 // The contents of this file is dual-licensed under the MIT or 0BSD license.
 
+//! A target-side implementation of the Real-Time Transfer protocol.
+//!
+//! The library provides a single writeable _up-channel_ for sending log data to
+//! the host. The functions in this library are also not reentrant; concurrent
+//! use must be protected with a critical section.
+//!
+//! Functions will block if the RTT buffer is full. If the host-side has
+//! disconnected and is no longer reading from the channel the microcontroller
+//! will lock up.
+//!
+//! # Examples
+//!
+//! ```zig
+//! const rtt = @import("rtt");
+//!
+//! export fn main(void) callconv(.C) noreturn {
+//!     rtt.println("Hello from {s}", .{"Zig!"});
+//!     while (true) {}
+//! }
+//! ```
+
 const std = @import("std");
 
+const atomic = std.atomic;
+const debug = std.debug;
 const fmt = std.fmt;
 
-/// The global control block.
+/// Print something to the RTT channel.
+///
+/// Uses the `std.fmt` plumbing under the hood.
+pub fn print(comptime fmt_str: []const u8, args: anytype) void {
+    _SEGGER_RTT.channel.print(fmt_str, args);
+}
+
+/// Print something to the RTT channel, with a newline.
+///
+/// Uses the `std.fmt` plumbing under the hood.
+pub fn println(comptime fmt_str: []const u8, args: anytype) void {
+    _SEGGER_RTT.channel.print(fmt_str, args);
+    _SEGGER_RTT.channel.writeAll("\n");
+}
+
+/// Write raw bytes directly to the RTT channel.
+///
+/// Does _not_ use the `std.fmt` plumbing.
+pub fn writeAll(bytes: []const u8) void {
+    _SEGGER_RTT.channel.writeAll(bytes);
+}
+
+const RTT_MODE_TRUNC = 1;
+const RTT_MODE_BLOCK = 2;
+
+const BUF_LEN = 256;
+var BUF: [BUF_LEN]u8 = undefined;
+
 export var _SEGGER_RTT: extern struct {
     magic: [16]u8,
     max_up_channels: usize,
@@ -18,36 +68,15 @@ export var _SEGGER_RTT: extern struct {
     .max_down_channels = 0,
     .channel = .{
         .name = "rtt.zig",
-        .buf = undefined,
-        .bufsz = 0,
+        .buf = &BUF,
+        .bufsz = BUF_LEN,
         .write = 0,
         .read = 0,
-        .flags = 0,
+        .flags = RTT_MODE_BLOCK,
     },
 };
 
-/// Initialize and acquire a pointer to the RTT channel.
-///
-/// This function must be called exactly once at the start of your firmware.
-pub fn init(comptime bufsz: usize, buf: *[bufsz]u8) *Channel {
-    var ch = &_SEGGER_RTT.channel;
-    ch.buf = buf;
-    ch.bufsz = bufsz;
-    return ch;
-}
-
-/// An RTT channel.
-///
-/// The `rtt.zig` library only provides writeable _up-channels_ for sending log
-/// data to the host.
-///
-/// The channel is not reentrant. Any concurrent use of the channel must be
-/// protected with a critical section.
-///
-/// Channel functions may block if the RTT buffer is full. If the host-side has
-/// disconnected and is no longer reading from the channel the microcontroller
-/// will lock up.
-pub const Channel = extern struct {
+const Channel = extern struct {
     name: [*]const u8,
     buf: [*]u8,
     bufsz: usize,
@@ -55,32 +84,45 @@ pub const Channel = extern struct {
     read: usize,
     flags: usize,
 
-    /// Print something to the RTT channel.
-    pub fn print(
+    fn print(
         self: *Channel,
         comptime fmt_str: []const u8,
         args: anytype,
     ) void {
-        const writer = Writer{ .channel = self };
+        const writer = Writer{ .chan = self };
         fmt.format(writer, fmt_str, args) catch unreachable;
     }
 
-    /// Write some bytes directly to the RTT channel.
-    pub fn writeAll(self: *Channel, bytes: []const u8) void {
-        const writer = Writer{ .channel = self };
+    fn writeAll(self: *Channel, bytes: []const u8) void {
+        const writer = Writer{ .chan = self };
         writer.writeAll(bytes) catch unreachable;
     }
 };
 
 const Writer = struct {
-    channel: *Channel,
+    chan: *Channel,
 
-    /// The writer is infallible.
-    pub const Error = error{};
+    pub const Error = error{}; // infallible
 
     pub fn writeAll(self: Writer, bytes: []const u8) Writer.Error!void {
-        _ = bytes;
-        _ = self;
+        var xs = bytes;
+
+        while (xs.len != 0) {
+            const write = readVolatile(&self.chan.write);
+            const read = readVolatile(&self.chan.read);
+            const avail = maxContiguous(read, write);
+
+            const n = @min(xs.len, avail);
+            if (n == 0) {
+                // todo: add non-blocking impl with truncated writes
+                debug.assert(readVolatile(&self.chan.flags) == RTT_MODE_BLOCK);
+                continue;
+            }
+
+            @memcpy(self.chan.buf[write .. write + n], xs[0..n]);
+            writeVolatile(&self.chan.write, (write + n) % BUF_LEN);
+            xs = xs[n..];
+        }
     }
 
     pub fn writeByteNTimes(
@@ -91,3 +133,20 @@ const Writer = struct {
         for (0..n) |_| self.writeAll(&[_]u8{byte}) catch unreachable;
     }
 };
+
+inline fn readVolatile(ptr: *const volatile usize) usize {
+    return ptr.*;
+}
+
+inline fn writeVolatile(ptr: *volatile usize, val: usize) void {
+    ptr.* = val;
+}
+
+inline fn maxContiguous(read: usize, write: usize) usize {
+    return if (read > write)
+        read - write - 1
+    else if (read == 0)
+        BUF_LEN - write - 1
+    else
+        BUF_LEN - write;
+}
